3d329251f8711718f14dfb3cbb299e8e615fd994
dev <d.gomezlanderolopez@gmail.com>
2025-11-19 19:53:20 +0100

    clean inception

---
+++ exam_rank_6/prueba.c
@@ -0,0 +1,66 @@
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct s_client{
+	int fd;
+	char msg[370000];
+}	t_client;
+
+t_client clients[2048];
+fd_set read_set, write_set, current;
+int max_fd = 0, gid = 0;
+char send_buffer[400000], recv_buffer[400000];
+
+void err(char *msg){
+	if (msg)
+		write(2, msg, strlen(msg));
+	else
+		write(2, "Fatal error", 11);
+	write(2, "\n",1);
+	exit(1);
+}
+
+void send_to_all(int except){
+	for(int fd = 0; fd <= max_fd; fd++){
+		if(FD_ISSET(fd, &write_set) && fd !=except)
+		{
+			if(send(fd, send_buffer, strlen(send_buffer),0) == -1)
+				err(NULL);
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		err("Wrong number of arguments");
+	struct sockaddr_in serveraddr;
+	socklen_t len = sizeof(serveraddr);
+	int serverfd = socket(AF_INET, SOCK_STREAM,0);
+
+	if(serverfd == -1)
+		err(NULL);
+	max_fd = serverfd;
+
+	FD_ZERO(&current);
+	FD_ZERO(&write_set);
+	FD_SET(serverfd, &current);
+
+	bzero(clients, sizeof(clients));
+	bzero(&serveraddr, sizeof(serveraddr));
+
+	serveraddr.sin_family = AF_INET;
+
+	serveraddr.sin_addr.s_addr = htonl(2130706433);
+	serveraddr.sin_port = htons(atoi(argv[1]));
+	if(bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(serverfd,100) == -1)
+			err(NULL);
+	while(1)
+	{
+	
+	}
+}
